525e8e8373ec4c1d9c8bb0a6acf18c34c3fd78c5
justsnails <loveserenasu@gmail.com>
2024-07-06 23:03:31 +0800

    lesson_5

---
+++ loop.py
@@ -0,0 +1,16 @@
+# loop 迴圈的英文
+# 1. while loop
+# 2. for loop
+print('跑步測驗開始，選手要跑10圈才算及格。')
+x = 1
+while x <= 10:
+    print('彥宇選手已經跑了第', x, '圈')
+    x += 1
+
+print('恭喜彥宇選手通過測驗')
+
+#while True :
+#    print('我困在迴圈中')
+#   break
+
+#print('我逃出迴圈了!')
